d66e3164583673408e85f287a662100ec1dee864
Arséni Pachkovski <arseni.pachkovski@hotmail.com>
2017-10-14 14:24:18 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Bienvenue dans mon application :D'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up :)');
+});
+
+//
+// app.get('/about', (req, res) => {
+//     res.send('My name is Arséni :D');
+// });
+//
+// app.get('/bad', (req, res) => {
+//     res.send({
+//         error : 'Whooooops'
+//     });
+// });
